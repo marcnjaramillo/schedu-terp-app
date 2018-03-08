013c6e76edf663be2ed5fa6e8746e67e271ce121
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-03-08 15:35:29 -0800

    Create index and show actions for Interpreter controller

--- app/controllers/interpreters_controller.rb
+++ app/controllers/interpreters_controller.rb
@@ -1,3 +1,13 @@
 class InterpretersController < ApplicationController
+  
+  def index
+    @interpreters = Interpreter.all
+    render json: @interpreters.to_json
+  end
+
+  def show
+    @interpreters = Interpreter.find(params[:id])
+    render json: @interpreters.to_json
+  end
 
 end
