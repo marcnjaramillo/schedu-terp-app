3691ca39e6d47b78f3282a3b9f888d615196c166
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-03-10 23:16:46 -0800

    Write functional component for Interpreters

--- client/src/components/Interpreters.js
+++ client/src/components/Interpreters.js
@@ -1,1 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+
+const Interpreters = ({ interpreters }) => {
+  const renderInterpreters = interpreters.map(interpreter =>
+    <div key={interpreter.id}>
+      <p>{interpreter.first_name + ' ' + interpreter.last_name}</p>
+      <p>{interpreter.street_address}</p>
+      <p>{interpreter.city + ', ' + interpreter.state + ' ' + interpreter.zip_code}</p>
+      <p>Certifications: {interpreter.certification}</p>
+      <p>Rate: {interpreter.rate}</p>
+    </div>
+  )
+
+  return (
+    <div>
+      {renderInterpreters}
+    </div>
+  )
+}
+
+export default Interpreters;
