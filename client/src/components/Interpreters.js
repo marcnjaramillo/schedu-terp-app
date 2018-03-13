d03bf209f3d3b29995e53558f19949c52349438a
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-03-12 19:16:31 -0700

    Edit mapping method on certifications

--- client/src/components/Interpreters.js
+++ client/src/components/Interpreters.js
@@ -8,8 +8,8 @@ const Interpreters = ({ interpreters }) => {
       <p>{interpreter.first_name + ' ' + interpreter.last_name}</p>
       <p>{interpreter.street_address}</p>
       <p>{interpreter.city + ', ' + interpreter.state + ' ' + interpreter.zip_code}</p>
-      <p>Certifications: {interpreter.certification}</p>
-      <p>Rate: {interpreter.rate}</p>
+      <p>Certifications: {interpreter.certifications.map(certification => <span className="Cert">{certification.name}</span>)}</p>
+      <p>Rate: {'$' + interpreter.rate}</p>
     </div>
   )
 
