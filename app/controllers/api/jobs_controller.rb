0d4526947ba0a5dc23d32cbfa25f7c9f40a0f2a5
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-18 23:31:52 -0700

    Complete job controller actions

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,16 +1,59 @@
 class Api::JobsController < ApplicationController
 
+before_action :set_job
+
   def index
     @jobs = Job.all
     render json: @jobs
   end
 
-  def show
-    @jobs = Job.find(params[:id])
-    render json: @jobs
-  end
-
   def create
-
+    job = Job.new(job_params)
+    if job.save
+      render json: job
+    else
+      render json: { message: job.errors }, status: 400
+    end
   end
+
+  def show
+    render json: @job
+  end
+
+  def update
+    if @job.update(job_params)
+      render json: @job
+    else
+      render json: { message: job.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @job.destroy
+      render json: { message: "Successfully deleted job." }, status: 204
+    else
+      render json: { message: "Unable to delete job." }, status: 400
+  end
+
+  private
+
+    def set_job
+      @job = Job.find_by(id: params[:id])
+    end
+
+    def job_params
+      params.require(:interpreter).permit(
+        :location,
+        :street_address,
+        :city,
+        :state,
+        :zip_code,
+        :contact_person,
+        :contact_number,
+        :nature,
+        :duration,
+        :client,
+        :notes
+      )
+    end
 end
