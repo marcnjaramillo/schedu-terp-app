8a9212ff009e43b22aeb2ea62d2827e9b7182ba1
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-21 00:08:35 -0700

    Clean up App and remove unnecessary code

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
 import Interpreters from './Interpreters';
-import InterpreterData from '../components/InterpreterData';
-import Jobs from './Jobs';
-import JobData from '../components/JobData';
 import './App.css';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      interpreters: [],
-      jobs: []
-    }
-  }
-
-  componentDidMount() {
-    InterpreterData.fetchInterpreters().then(interpreters => this.setState({ interpreters }))
-    JobData.fetchJobs().then(jobs => this.setState({ jobs }))
-  }
 
   render() {
     return (
       <div className="App">
         <h1>ScheduTerp</h1>
         <div className="Interpreters">
-          <Interpreters interpreters={this.state.interpreters} />
+          <Interpreters />
         </div>
-        <div className="Jobs">
-          <Jobs jobs={this.state.jobs} />
-        </div>
+
       </div>
     );
   }
