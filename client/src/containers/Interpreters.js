4be235b2b3a10526b06a07360244d04d0c3faea5
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-21 00:20:09 -0700

    Connect app to back-end API

--- client/src/containers/Interpreters.js
+++ client/src/containers/Interpreters.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getInterpreters } from '../actions/interpreters';
 
 import InterpreterPanel from '../components/InterpreterPanel';
 
 
-const Interpreters = (props) => (
-  <div className="InterpretersContainer">
-    <h1>Interpreters</h1>
-    {props.interpreters.map(interpreter => <InterpreterPanel key={interpreter.id} interpreter={interpreter} />)}
-  </div>
-);
 
-export default Interpreters;
+class Interpreters extends Component {
+
+  componentDidMount() {
+    this.props.getInterpreters()
+  }
+
+  render () {
+    return (
+      <div className="InterpretersContainer">
+        <h1>Interpreters</h1>
+        {this.props.interpreters.map(interpreter => <InterpreterPanel key={interpreter.id} interpreter={interpreter} />)}
+
+      </div>
+
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return({
+    interpreters: state.interpreters
+  })
+}
+
+export default connect(mapStateToProps, { getInterpreters })(Interpreters);
