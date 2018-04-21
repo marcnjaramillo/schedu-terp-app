c04828151223f32b853d43b6a2871c53580dbdd9
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-21 00:09:44 -0700

    Refactor code

--- client/src/components/InterpreterPanel.js
+++ client/src/components/InterpreterPanel.js
@@ -5,9 +5,9 @@ const InterpreterPanel = ({ interpreter }) => (
     <p>{interpreter.first_name + ' ' + interpreter.last_name}</p>
     <p>{interpreter.street_address}</p>
     <p>{interpreter.city + ', ' + interpreter.state + ' ' + interpreter.zip_code}</p>
-    <p>Certifications: {interpreter.certifications.map(certification => <span className="Cert">{certification.name}</span>)}</p>
-    <p>Rate: {'$' + interpreter.rate}</p>
   </div>
 )
 
 export default InterpreterPanel;
+
+// TODO: Figure out how to associate certification with an interpreter.
