c71a12fc4f253f01022e1798be9b4f941c4082b7
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-26 12:01:05 -0700

    Create routes

--- client/src/containers/Interpreters.js
+++ client/src/containers/Interpreters.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Route, Switch } from 'react-router-dom';
 import InterpreterPanel from '../components/InterpreterPanel';
 import InterpreterForm from './InterpreterForm';
+import Interpreter from '../components/Interpreter';
 import { getInterpreters } from '../actions/interpreters';
 import './Interpreters.css';
 
@@ -14,11 +15,21 @@ class Interpreters extends Component {
   }
 
   render () {
+    const { match, interpreters } = this.props;
+
     return (
-      <div className="InterpretersContainer">
-        <h1>Interpreters</h1>
-        {this.props.interpreters.map(interpreter => <InterpreterPanel key={interpreter.id} interpreter={interpreter} />)}
-        <InterpreterForm/>
+      <div>
+        <Switch>
+          <Route path={`${match.url}/new`} component={InterpreterForm} />
+          <Route path={`${match.url}/:interpreterId`} component={Interpreter} />
+
+          <Route exact path={`${match.url}`} render={() => (
+            <div className="InterpretersContainer">
+              <h1>Interpreters</h1>
+              {interpreters.map(interpreter => <InterpreterPanel key={interpreter.id} match={match} interpreter={interpreter} />)}
+            </div>
+          )} />
+        </Switch>
       </div>
 
     );
