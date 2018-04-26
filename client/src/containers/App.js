02c976dc2c77ef13d41450db9f876c84dfb9f05f
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-26 11:58:40 -0700

    Connect react-router and add routes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Interpreters from './Interpreters';
 import './App.css';
+
+const Home = () => <h1>Welcome to ScheduTerp</h1>;
 
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>ScheduTerp</h1>
-        <div className="Interpreters">
-          <Interpreters />
-        </div>
+      <Router>
+        <div className="App">
+          <div className="Navbar">
+            <Link to="/">Home</Link>
+            <Link to="/interpreters">Interpreters</Link>
+            <Link to="/interpreters/new">Add An Interpreter</Link>
+          </div>
 
-      </div>
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/interpreters" component={Interpreters} />
+        </div>
+      </Router>
     );
   }
 }
