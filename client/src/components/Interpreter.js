804daa4a30f1a0ec3431904bafc273c15826f0cd
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-26 12:03:07 -0700

    Create new Interpreter page to show full details

---
+++ client/src/components/Interpreter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Interpreter = ({ interpreter }) => {
+
+  return (
+    <div>
+      <h3>{interpreter.first_name + ' ' + interpreter.last_name}</h3>
+      <p>Address: {interpreter.street_address}</p>
+      <p>{interpreter.city + ', ' + interpreter.state + ' ' + interpreter.zip_code}</p>
+    </div>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => {
+
+  const interpreter = state.interpreters.find(interpreter => interpreter.id === ownProps.match.params.interpreterId)
+
+  if (interpreter) {
+    return { interpreter }
+  } else {
+    return { interpreter: {} }
+  }
+}
+
+export default connect(mapStateToProps)(Interpreter);
