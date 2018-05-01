762f7313eb1339ad1cd8adba8768fb7e52d55141
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-30 17:24:22 -0700

    Add new Job routes

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,12 +3,11 @@ import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import Home from '../components/Home';
 import Interpreters from './Interpreters';
+import Jobs from './Jobs';
 import './App.css';
-
-const Home = () => <h1>Welcome to ScheduTerp</h1>;
-
 
 class App extends Component {
 
@@ -20,12 +19,15 @@ class App extends Component {
             <Link to="/">Home</Link>
             <Link to="/interpreters">Interpreters</Link>
             <Link to="/interpreters/new">Add An Interpreter</Link>
+            <Link to="/jobs">Jobs</Link>
+            <Link to="/jobs/new">Add A Job</Link>
           </div>
 
           <hr />
 
           <Route exact path="/" component={Home} />
           <Route path="/interpreters" component={Interpreters} />
+          <Route path="/jobs" component={Jobs} />
         </div>
       </Router>
     );
