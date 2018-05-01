3d31ef49e07a6c070c037982cfb64959166ee80d
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-04-30 17:30:49 -0700

    Connect jobs to store

--- client/src/store.js
+++ client/src/store.js
@@ -6,16 +6,21 @@ import {
 import thunk from 'redux-thunk';
 import interpreters from './reducers/interpreters';
 import interpreterFormData from './reducers/interpreterFormData';
+import jobs from './reducers/jobs';
+import jobFormData from './reducers/jobFormData';
+
 
 const reducers = combineReducers({
   interpreters,
-  interpreterFormData
+  interpreterFormData,
+  jobs,
+  jobFormData
 });
 
 const middleWare = [thunk];
 
 export default createStore(
   reducers,
-  window.__REDUX__DEVTOOLS__EXTENSION__ && window.__REDUX__DEVTOOLS__EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(...middleWare)
 );
