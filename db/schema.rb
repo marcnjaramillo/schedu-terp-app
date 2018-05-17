291db59c6ee445cd9ce8eac5c02bb782aefb3e5f
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-05-16 23:47:14 -0700

    Switch to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180328205229) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "certifications", force: :cascade do |t|
     t.integer "interpreter_id"
     t.string "name"
