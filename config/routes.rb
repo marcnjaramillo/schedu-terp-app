4f8631e912c0f3c4d6f9800d252497d860f894f6
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-07-01 10:46:36 -0700

    Update API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :profiles
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :interpreters
+    resources :profiles
+    resources :users
     resources :jobs
   end
 end
