c1a829c5da2a1b52af8b03cc994fb05d06193e4b
marcnjaramillo <marcn.jaramillo@gmail.com>
2018-07-01 10:41:11 -0700

    Add profile and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :profiles
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :interpreters
